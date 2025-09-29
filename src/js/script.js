45ecfde29b1fb2d105428920ccf0ab3f54b4b404
YarceJose <tamayoyarce@gmail.com>
2025-09-29 13:25:27 -0500

    Comienzo de la estructura del proyecto integrado

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+const carrusel = document.querySelector(".carrusel-contenedor");
+const items = document.querySelectorAll(".carrusel-item");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicadores = document.querySelector(".indicadores");
+
+let index = 0;
+
+// Crear indicadores
+items.forEach((_, i) => {
+  let dot = document.createElement("div");
+  if (i === 0) dot.classList.add("activo");
+  dot.addEventListener("click", () => moverCarrusel(i));
+  indicadores.appendChild(dot);
+});
+
+function actualizarIndicadores() {
+  document.querySelectorAll(".indicadores div").forEach((dot, i) => {
+    dot.classList.toggle("activo", i === index);
+  });
+}
+
+function moverCarrusel(i) {
+  index = i;
+  carrusel.style.transform = `translateX(-${index * 100}%)`;
+  actualizarIndicadores();
+}
+
+prev.addEventListener("click", () => {
+  index = index > 0 ? index - 1 : items.length - 1;
+  moverCarrusel(index);
+});
+
+next.addEventListener("click", () => {
+  index = index < items.length - 1 ? index + 1 : 0;
+  moverCarrusel(index);
+}); 
